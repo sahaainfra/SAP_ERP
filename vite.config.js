a598dcc2e4f5d5bdf6786c00eacc24baef970a7a
sahaainfra <sahaainfra@gmail.com>
2026-09-09 13:09:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,6 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 3000,
-    strictPort: true,
-    hmr: {
-      port: 3000,
-    },
+    strictPort: false, // CHANGED TO FALSE
   },
 });
